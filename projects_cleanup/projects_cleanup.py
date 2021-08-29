f81f9dc8fa3a8e98532411b0aa454e3c9d4cef20
tamari-oz <72387668+tamari-oz@users.noreply.github.com>
2021-08-29 16:13:38 +0300

    Performance Enhancement - Added parallelism for fetching project from products (#19)
    * Performance Enhancement - Added parallelism for fetching project from products
    
    * Update tool version

--- projects_cleanup/projects_cleanup.py
+++ projects_cleanup/projects_cleanup.py
@@ -12,7 +12,7 @@ from ws_sdk.web import WS
 file_handler = logging.FileHandler(filename='cleanup.log')
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [file_handler, stdout_handler]
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG if os.environ.get("DEBUG") else logging.INFO,
                     format='%(levelname)s %(asctime)s %(thread)d: %(message)s',
                     handlers=handlers
                     )
@@ -28,7 +28,7 @@ INVALID_CHARS = ['\\', '<', '>', ':', '"', '/', '|', '?', '*']
 
 PS = "ps-"
 AGENT_NAME = "cleanup-tool"
-AGENT_VERSION = "0.1.5"
+AGENT_VERSION = "0.1.7"
 
 
 def replace_invalid_chars(directory: str) -> str:
@@ -47,43 +47,58 @@ def get_reports_to_archive() -> tuple:
     days_to_keep = timedelta(days=config.getint('DEFAULT', 'DaysToKeep'))
     archive_date = datetime.utcnow() - days_to_keep
     logger.info(f"Keeping {days_to_keep.days} days. Archiving projects older than {archive_date}")
+    projects_to_archive, project_report_desc_list = get_projects_and_reports(archive_date, products)
 
-    all_projects = []
+    return projects_to_archive, project_report_desc_list
+
+
+def get_projects_and_reports(archive_date, products) -> tuple:
+    manager = Manager()
+    projects_to_archive_q = manager.Queue()
+    project_report_desc_list_q = manager.Queue()
+    with ThreadPool(processes=project_parallelism_level) as pool:
+        pool.starmap(get_prod_projects_and_reports_w, [(archive_date, prod, c_org, projects_to_archive_q, project_report_desc_list_q) for prod in products])
+
+    projects_to_archive = []
+    while not projects_to_archive_q.empty():
+        projects_to_archive.append(projects_to_archive_q.get(block=True, timeout=0.05))
+
     project_report_desc_list = []
-    for prod in products:  # Creating list of all reports of all projects to be produced
-        curr_prod_proj_to_archive = []
-        curr_prod_projects = c_org.get_projects(product_token=prod['token'])
-        logger.info(f"Handling product: {prod['name']} number of projects: {len(curr_prod_projects)}")
-        for project in curr_prod_projects:
-            project_time = datetime.strptime(project['lastUpdatedDate'], "%Y-%m-%d %H:%M:%S +%f")
-            if project_time < archive_date:
-                logger.debug(
-                    f"Project {project['name']} Token: {project['token']} Last update: {project['lastUpdatedDate']} will be archived")
+    while not project_report_desc_list_q.empty():
+        project_report_desc_list.append(project_report_desc_list_q.get(block=True, timeout=0.05))
 
-                # Characters validation
-                product_name = replace_invalid_chars(project['productName'])
-                project_name = replace_invalid_chars(project['name'])
+    logger.info(f"Found total {len(projects_to_archive)} projects to archive ({len(project_report_desc_list)} reports will be produced)")
 
-                project['project_archive_dir'] = os.path.join(os.path.join(archive_dir, product_name), project_name)
-                curr_prod_proj_to_archive.append(project)
+    return projects_to_archive, project_report_desc_list
 
-        logger.info(f"Found {len(curr_prod_proj_to_archive)} projects to archive on product: {prod['name']}")
 
-        for project in curr_prod_proj_to_archive:  # Creating list of report to-be-produced meta data
-            if not os.path.exists(project['project_archive_dir']):
-                os.makedirs(project['project_archive_dir'])
-            for report_type in report_types.keys():
-                project_report = project.copy()
-                project_report['report_type'] = report_type
-                project_report['report_full_name'] = os.path.join(project_report['project_archive_dir'],
-                                                                  report_types[report_type])
-                project_report_desc_list.append(project_report)
+def get_prod_projects_and_reports_w(archive_date, prod, ws_conn, projects_to_archive_q, project_report_desc_list_q):
+    curr_prod_proj_to_archive = []
+    curr_prod_projects = ws_conn.get_projects(product_token=prod['token'])
+    logger.info(f"Handling product: {prod['name']} number of projects: {len(curr_prod_projects)}")
 
-        all_projects = all_projects + curr_prod_proj_to_archive
-    logger.info(
-        f"Found total {len(all_projects)} projects to archive ({len(project_report_desc_list)} reports will be produced)")
+    for project in curr_prod_projects:
+        project_time = datetime.strptime(project['lastUpdatedDate'], "%Y-%m-%d %H:%M:%S +%f")
+        if project_time < archive_date:
+            logger.debug(f"Project {project['name']} Token: {project['token']} Last update: {project['lastUpdatedDate']} will be archived")
+            # Characters validation
+            product_name = replace_invalid_chars(project['productName'])
+            project_name = replace_invalid_chars(project['name'])
+            project['project_archive_dir'] = os.path.join(os.path.join(archive_dir, product_name), project_name)
+            curr_prod_proj_to_archive.append(project)
+            projects_to_archive_q.put(project)
 
-    return all_projects, project_report_desc_list
+    logger.info(f"Found {len(curr_prod_proj_to_archive)} projects to archive on product: {prod['name']}")
+
+    for project in curr_prod_proj_to_archive:  # Creating list of report to-be-produced meta data
+        if not os.path.exists(project['project_archive_dir']):
+            os.makedirs(project['project_archive_dir'])
+        for report_type in report_types.keys():
+            project_report = project.copy()
+            project_report['report_type'] = report_type
+            project_report['report_full_name'] = os.path.join(project_report['project_archive_dir'],
+                                                              report_types[report_type])
+            project_report_desc_list_q.put(project_report)
 
 
 def generate_reports_manager(reports_desc_list: list) -> list:
@@ -94,9 +109,9 @@ def generate_reports_manager(reports_desc_list: list) -> list:
         pool.starmap(worker_generate_report,
                      [(report_desc, c_org, failed_proj_tokens_q) for report_desc in reports_desc_list])
 
-    failed_projects = set()
+    failed_projects = []
     while not failed_proj_tokens_q.empty():
-        failed_projects.add(failed_proj_tokens_q.get(block=True, timeout=0.05))
+        failed_projects.append(failed_proj_tokens_q.get(block=True, timeout=0.05))
 
     if failed_projects:
         logger.warning(f"{len(failed_projects)} projects were failed to archive")
@@ -105,8 +120,7 @@ def generate_reports_manager(reports_desc_list: list) -> list:
 
 
 def worker_generate_report(report_desc: dict, connector: WS, w_f_proj_tokens_q) -> None:
-    logger.debug(
-        f"Running report {report_desc['report_type']} on project: {report_desc['name']}. location: {report_desc['report_full_name']}")
+    logger.debug(f"Running report {report_desc['report_type']} on project: {report_desc['name']}. location: {report_desc['report_full_name']}")
     method_name = f"get_{report_desc['report_type']}"
     try:
         method_to_call = getattr(WS, method_name)
@@ -121,23 +135,22 @@ def worker_generate_report(report_desc: dict, connector: WS, w_f_proj_tokens_q) 
     except AttributeError:
         logger.error(f"report: {method_name} was not found")
     except Exception:
-        logger.exception(
-            f"Error producing report: {report_desc['report_type']} on project {report_desc['name']}. Project will not be deleted.")
+        logger.exception(f"Error producing report: {report_desc['report_type']} on project {report_desc['name']}. Project will not be deleted.")
         w_f_proj_tokens_q.put(report_desc['token'])
 
 
-def delete_projects(proj_to_archive: list, failed_project_toks: list) -> None:
-    projects_to_delete = proj_to_archive.copy()
-    for project in proj_to_archive:
+def delete_projects(projects_to_archive: list, failed_project_toks: list) -> None:
+    projects_to_delete = projects_to_archive.copy()
+    for project in projects_to_archive:
         if project['token'] in failed_project_toks:
             projects_to_delete.remove(project)
-    logger.info(f"Out of {len(proj_to_archive)} projects, {len(projects_to_delete)} projects will be deleted")
+    logger.info(f"Out of {len(projects_to_archive)} projects, {len(projects_to_delete)} projects will be deleted")
 
     if projects_to_delete:
         global dry_run, c_org
         with ThreadPool(processes=1) as thread_pool:
             thread_pool.starmap(worker_delete_project, [(c_org, project, dry_run) for project in projects_to_delete])
-        logger.info(f"{len(proj_to_archive)} projects deleted")
+        logger.info(f"{len(projects_to_archive)} projects deleted")
 
 
 def worker_delete_project(conn, project, w_dry_run):
@@ -190,7 +203,7 @@ if __name__ == '__main__':
                tool_details=(PS + AGENT_NAME, AGENT_VERSION))
     if dry_run:
         logger.info("Running in DRY_RUN mode. Project will not be deleted and reports will not be generated!!!")
-    projects_to_archive, reports_to_archive = get_reports_to_archive()
+    proj_to_archive, reports_to_archive = get_reports_to_archive()
     failed_project_tokens = []
     if config['DEFAULT'].getboolean('SkipReportGeneration', False):
         logger.info("Skipping Report Generation")
@@ -199,6 +212,6 @@ if __name__ == '__main__':
     if config['DEFAULT'].getboolean('SkipProjectDeletion', False):
         logging.info("Skipping Project Deletion")
     else:
-        delete_projects(projects_to_archive, failed_project_tokens)
+        delete_projects(proj_to_archive, failed_project_tokens)
 
     logger.info(f"Project Cleanup finished. Run time: {datetime.now() - start_time}")
