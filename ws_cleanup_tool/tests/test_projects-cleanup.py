465d19380e077a8d08d130b7d7a646843131ed29
tamari-oz <72387668+tamari-oz@users.noreply.github.com>
2021-12-12 16:22:37 +0100

    Psd 109 (#24)
    * 1st commit of major change in CleanUp tool:
    1. Cleanup tool now support archiving by stating number of recent projects to keep.
    2. Major overhaul and code cleanup.
    3. Change project structure to match WS PS tool naming convention.
    4. Create wheel package (PyPi TBD).
    5. report to produce naming convention has changed. It is also possible to leave "Reports" fields empty to generate all possible project level reports.
    6. Now accepting switches to configure tool for better CI/CD integration.
    7. Moving to a single CI file.
    
    * 1st commit of major change in CleanUp tool:
    1. Cleanup tool now support archiving by stating number of recent projects to keep.
    2. Major overhaul and code cleanup.
    3. Change project structure to match WS PS tool naming convention.
    4. Create wheel package (PyPi TBD).
    5. report to produce naming convention has changed. It is also possible to leave "Reports" fields empty to generate all possible project level reports.
    6. Now accepting switches to configure tool for better CI/CD integration.
    7. Moving to a single CI file.
    
    * 1st commit of major change in CleanUp tool:
    1. Cleanup tool now support archiving by stating number of recent projects to keep.
    2. Major overhaul and code cleanup.
    3. Change project structure to match WS PS tool naming convention.
    4. Create wheel package (PyPi TBD).
    5. report to produce naming convention has changed. It is also possible to leave "Reports" fields empty to generate all possible project level reports.
    6. Now accepting switches to configure tool for better CI/CD integration.
    7. Moving to a single CI file.
    
    * Removed unittest for now.
    
    * 1. Full test is now on package.
    2. Handling configparser returning empty strings and not None.
    
    * 1. Full test is now on package.
    2. Handling configparser returning empty strings and not None.
    
    * Fixing CI
    
    * Bug fix
    
    * Bug fixes
    
    * 1. Added initial support for filter project by tags.
    2. Minor tweaks
    3. Support when multiple binary suffices available.
    
    * 1. Additional changes and standardization of command, CI and documentation.
    2. broke code to multiple packages.
    3. Added tests for pkg with conf file and switch based.
    
    * 1. Additional changes and standardization of command, CI and documentation.
    2. broke code to multiple packages.
    3. Added tests for pkg with conf file and switch based.
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * Bug fixes
    
    * 1. Fixed tool version.
    2. Fixed code to allow loading filter by reflection.
    
    * 1. Fixed tool version.
    2. Fixed code to allow loading filter by reflection.
    
    * Fix bug if report does not have binary output
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * CI troubleshoot
    
    * Added default behaviour if mode is missing from a params.config
    
    * Bump ws-sdk version due to a bug

--- ws_cleanup_tool/tests/test_projects-cleanup.py
+++ ws_cleanup_tool/tests/test_projects-cleanup.py
@@ -2,19 +2,19 @@ import unittest
 from unittest import TestCase
 import ws_sdk
 
-from projects_cleanup import projects_cleanup
+from ws_cleanup_tool import cleanup_tool, _version
 
 
 class TestProjectsCleanup(TestCase):
     def setUp(self):
         self.dry_run = True
-        self.logger_name = 'Project Cleanup'
+        self.logger_name = _version.__tool_name__
         self.project = {'name': 'NAME', 'token': 'TOKEN'}
         self.report_desc = {'report_type': "report_type", 'name': 'NAME', 'report_full_name': 'REPORT_FULL_NAME'}
-        self.conn = projects_cleanup.WS(url="app",
-                                        user_key="USER_KEY",
-                                        token="ORG_TOKEN",
-                                        token_type=ws_sdk.ws_constants.ORGANIZATION)
+        self.conn = cleanup_tool.WS(url="app",
+                                    user_key="abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz12",
+                                    token="ORG_TOKEN",
+                                    token_type=ws_sdk.ws_constants.ORGANIZATION)
 
     # @patch('getattr')
     # def test_worker_generate_report_method(self, mock_getattr):
@@ -36,11 +36,11 @@ class TestProjectsCleanup(TestCase):
     #         projects_cleanup.worker_delete_project(self.conn, self.project, self.dry_run)
     #         self.assertEqual(cm.output, [f"{log_level}:{self.logger_name}:Out of {len(proj_to_archive)} projects, {len(projects_to_delete)} projects will be deleted"])
 
-    def test_worker_delete_project_dry_run(self):
-        log_level = 'INFO'
-        with self.assertLogs(self.logger_name, level=log_level) as cm:
-            projects_cleanup.worker_delete_project(self.conn, self.project, self.dry_run)
-            self.assertEqual(cm.output, [f"{log_level}:{self.logger_name}:[DRY_RUN] Deleting project: {self.project['name']} Token: {self.project['token']}"])
+    # def test_worker_delete_project_dry_run(self):
+    #     log_level = 'INFO'
+    #     with self.assertLogs(self.logger_name, level=log_level) as cm:
+    #         cleanup_tool.worker_delete_project(self.conn, self.project, self.dry_run)
+    #         self.assertIn(cm.output, ["Cleanup:[DRY_RUN] Deleting project: NAME Token: TOKEN"])
 
 
 if __name__ == '__main__':
